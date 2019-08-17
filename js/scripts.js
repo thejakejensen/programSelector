cd0eb6284b6e123e188b4e1ed85f62ea5bc91381
jacob jensen <thejakejensen@gmail.com>
2019-08-17 13:18:26 -0700

    update more trials to make javascript work

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-  $("#langs").hide()
-
-  $("#formOne").submit(function(event) {
+  $("#jazz").submit(function(event) {
     event.preventDefault()
 
     var hero = parseInt($("#hero").val());
@@ -10,24 +8,25 @@ $(document).ready(function() {
     var colour = parseInt($("#colour").val());
     var animal = parseInt($("#animal").val());
 
-    var score = food + shoe + hero + colour + animal;
+    var score = function()}
+    return food + shoe + hero + colour + animal ;
 
     if (score > 5) {
       $("#ruby").hide();
       $("#python").hide();
       $("#Rust").hide();
-      console.log(hi)
-    } else if (score >= 5 && score < 7) {
+
+    } else if (score >= 5 && score <= 7) {
       $("#ruby").show();
       $("#python").hide();
       $("#Rust").hide();
 
-    } else if (score >= 7 && score < 13) {
+    } else if (score === 8 && score <= 13) {
       $("#ruby").hide();
       $("#python").show();
       $("#Rust").hide();
 
-    } else if (score >= 13 && score < 20) {
+    } else if (score >= 13 && score <= 20) {
       $("#ruby").hide();
       $("#python").hide();
       $("#Rust").show();
