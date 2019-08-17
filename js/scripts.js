7e3c242b704a3333e05e3be830dda45ef89ef750
jacob jensen <thejakejensen@gmail.com>
2019-08-17 12:10:32 -0700

    Still really stuck trying to fix my javascript, really not understanding

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,38 @@
-
 $(document).ready(function() {
+  $("#langs").hide()
+
   $("#formOne").submit(function(event) {
     event.preventDefault()
-    $("#langs").hide()
+
     var hero = parseInt($("#hero").val());
     var shoe = parseInt($("#shoe").val());
     var food = parseInt($("#food").val());
     var colour = parseInt($("#colour").val());
     var animal = parseInt($("#animal").val());
 
-    var score = food + shoe + hero + colour + animal ;
+    var score = food + shoe + hero + colour + animal;
 
-    if (score < 5){
-      $(".ruby").hide();
-      $(".python").hide();
-      $(".Rust").hide();
+    if (score > 5) {
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#Rust").hide();
+      console.log(hi)
+    } else if (score >= 5 && score < 7) {
+      $("#ruby").show();
+      $("#python").hide();
+      $("#Rust").hide();
 
-    } else if (score >=5 && score <7){
-      $(".ruby").show();
-      $(".python").hide();
-      $(".Rust").hide();
+    } else if (score >= 7 && score < 13) {
+      $("#ruby").hide();
+      $("#python").show();
+      $("#Rust").hide();
 
-    } else if (score >=7 && score <13){
-      $(".ruby").hide();
-      $(".python").show();
-      $(".Rust").hide();
-
-    } else if (score >=13 && score <20){
-      $(".ruby").hide();
-      $(".python").hide();
-      $(".Rust").show();
+    } else if (score >= 13 && score < 20) {
+      $("#ruby").hide();
+      $("#python").hide();
+      $("#Rust").show();
 
     };
-    console.log(score)
 
-    console.log(score)
   });
 });
